abecdef21d41d0ead3218c9e02598b33f7e37f87
pratimaACHARYA <60184548+pratimahrya@users.noreply.github.com>
2024-06-27 12:20:05 +0545

    second commit

--- test_todo_api.py
+++ test_todo_api.py
@@ -2,41 +2,62 @@ import requests
 
 ENDPOINT = "https://todo.pixegami.io"
 
-def test_can_call_endpoint():
-    response = requests.get(ENDPOINT)
-    assert response.status_code == 200
+# Helper functions
+def create_task(payload):
+    return requests.put(ENDPOINT + "/create-task", json=payload)
 
+def update_task(payload):
+    return requests.put(ENDPOINT + "/update-task", json=payload)
 
-def test_can_create_task():
-    payload = {
+def get_task(task_id):
+    return requests.get(ENDPOINT + f"/get-task/{task_id}")
+
+def new_task_payload():
+    return {
         "content": "my test content",
         "user_id": "test_user",
         "is_done": False
     }
-    create_task_response = requests.put(ENDPOINT + "/create-task", json=payload)
-    assert create_task_response.status_code == 200
 
+# Tests
+def test_can_call_endpoint():
+    response = requests.get(ENDPOINT)
+    assert response.status_code == 200
+
+def test_can_create_task():
+    payload = new_task_payload()
+    create_task_response = create_task(payload)
+    assert create_task_response.status_code == 200
     data = create_task_response.json()
-    print(data)
 
     task_id = data["task"]["task_id"]
     get_task_response = requests.get(ENDPOINT + f"/get-task/{task_id}")
 
     assert get_task_response.status_code == 200
     get_task_data = get_task_response.json()
-    print(get_task_data)
     assert get_task_data["content"] == payload["content"]
     assert get_task_data["user_id"] == payload["user_id"]
 
+def test_can_update_task():
+    # Create a task
+    payload = new_task_payload()
+    create_task_response = create_task(payload)
+    assert create_task_response.status_code == 200
+    task_id = create_task_response.json()["task"]["task_id"]
 
-    def test_can_update_task():
-        #create a task
-        #update the task
-        #get and validate the changes
-        pass
+    # Update the task
+    new_payload = {
+        "user_id": payload["user_id"],
+        "task_id": task_id,
+        "content": "my updated test content",
+        "is_done": True
+    }
+    update_task_response = update_task(new_payload)
+    assert update_task_response.status_code == 200
 
-    def create_task(payload):
-         return requests.put(ENDPOINT + "/create-task", json=payload)
-
-    def get_task(task_id):
-        return requests.get(ENDPOINT + f"/get-task/{task_id}")
+    # Get and validate the changes
+    get_task_response = get_task(task_id)
+    assert get_task_response.status_code == 200
+    get_task_data = get_task_response.json()
+    assert get_task_data["content"] == new_payload["content"]
+    assert get_task_data["is_done"] == new_payload["is_done"]
